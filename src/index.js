bb2d4dd298f8343209d97568b72f4bb08539bd08
Stephen Zacarias <stephenzacarias2@gmail.com>
2023-11-24 19:20:33 -0800

    Deploy Github page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './pages/App';
+import KeySongs from './components/keySongs';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App>
+    </App>
   </React.StrictMode>
 );
 
