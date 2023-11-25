9f9dad016678c3dc9ea3522b4dd6fd28e1a62c19
Stephen Zacarias <stephenzacarias2@gmail.com>
2023-11-24 22:34:57 -0800

    Styling

--- src/components/keySongs.jsx
+++ src/components/keySongs.jsx
@@ -6,7 +6,7 @@ function KeySongs(props) {
     const { songsList, songKey } = props;
 
     return (
-        <div className="keySong" key={songsList}>
+        <div key={songsList}>
             <h2 className='key-header'>Songs in {songKey}:</h2>
                 <ul className="song-list">
                     {songsList.map((song) => {
@@ -14,7 +14,7 @@ function KeySongs(props) {
                     })}
                 </ul>
         </div>
-    )
+    );
 }
 
 export default KeySongs;
--- src/pages/SongsPage.jsx
+++ src/pages/SongsPage.jsx
@@ -1,4 +1,3 @@
-// import logo from './logo.svg';
 import './SongsPage.css';
 import KeySongs from '../components/keySongs';
 import songData from "../song-data.json";
@@ -24,30 +23,30 @@ function SongsPage() {
       <header className="app-page">
         <h1 id="app-header">Song Keys</h1>
         <p id="site-description">
-        A website to display the musical keys of songs that I've been curating.
-        When I listen to music, I like to figure out the key of the song in concert pitch.
-        Then, I use a Python script that calls the Spotify API, and stores the song information associated
-        with the musical key I provide, into a JSON file, which then gets displayed here.
-        Check out which songs I've figured out the key to! :)
+          A website to display the musical keys of songs that I've been curating.
+          When I listen to music, I like to figure out the key of the song in concert pitch.
+          Then, I use a Python script that calls the Spotify API, and stores the song information associated
+          with the musical key I provide, into a JSON file, which then gets displayed here.
+          Check out which songs I've figured out the key to! :)
         </p>
         
 
-        <KeySongs songsList={BbSongs} songKey={"Bb"}></KeySongs>
-        <KeySongs songsList={BSongs} songKey={"B"}></KeySongs>
-        <KeySongs songsList={CSongs} songKey={"C"}></KeySongs>
-        <KeySongs songsList={DbSongs} songKey={"Db"}></KeySongs>
-        <KeySongs songsList={DSongs} songKey={"D"}></KeySongs>
-        <KeySongs songsList={EbSongs} songKey={"Eb"}></KeySongs>
-        <KeySongs songsList={ESongs} songKey={"E"}></KeySongs>
-        <KeySongs songsList={FSongs} songKey={"F"}></KeySongs>
-        <KeySongs songsList={GbSongs} songKey={"Gb"}></KeySongs>
-        <KeySongs songsList={GSongs} songKey={"G"}></KeySongs>
-        <KeySongs songsList={AbSongs} songKey={"Ab"}></KeySongs>
-        <KeySongs songsList={ASongs} songKey={"A"}></KeySongs>
+        <KeySongs songsList={BbSongs} songKey={"Bb"}/>
+        <KeySongs songsList={BSongs} songKey={"B"}/>
+        <KeySongs songsList={CSongs} songKey={"C"}/>
+        <KeySongs songsList={DbSongs} songKey={"Db"}/>
+        <KeySongs songsList={DSongs} songKey={"D"}/>
+        <KeySongs songsList={EbSongs} songKey={"Eb"}/>
+        <KeySongs songsList={ESongs} songKey={"E"}/>
+        <KeySongs songsList={FSongs} songKey={"F"}/>
+        <KeySongs songsList={GbSongs} songKey={"Gb"}/>
+        <KeySongs songsList={GSongs} songKey={"G"}/>
+        <KeySongs songsList={AbSongs} songKey={"Ab"}/>
+        <KeySongs songsList={ASongs} songKey={"A"}/>
 
       </header>
     </div>
-  )
+  );
 
 }
 
